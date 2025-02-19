ab86a34480025f1c4110c6f1e56807135d257b48
Hikiru <18605854+Hikiru@users.noreply.github.com>
2025-02-19 16:51:58 -0600

    stuff

--- 2023A/alan.java
+++ 2023A/alan.java
@@ -4,6 +4,12 @@ import java.io.FileNotFoundException;
 
 public class alan {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scoobydoo = new Scanner(new File("alan.dat"));
+        Scanner temp = new Scanner(new File("2023A/alan.dat"));
+        int F = temp.nextInt();
+        while (temp.hasNext()) {
+            F = temp.nextInt();
+            double K = (5 / 9) * (F - 32) + 273.15;
+            System.out.println(F + " degrees Fahrenheit is equal to " + K + " Kelvin");
+        }
     }
 }
